310cef0632321f8ff449ef3e9f980d985f631879
RobertoSilva1 <69914704+RobertoSilva1@users.noreply.github.com>
2020-12-10 11:29:44 -0300

    updates

--- src/components/dashboard/Cards/PostCard2.js
+++ src/components/dashboard/Cards/PostCard2.js
@@ -1,11 +1,9 @@
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { Wrapper} from './../../../components/dashboard'
--- src/screens/dashboard/admin/DAdminUsers.js
+++ src/screens/dashboard/admin/DAdminUsers.js
@@ -11,10 +11,8 @@ import LastPage from "@material-ui/icons/LastPage";
 import Add from "@material-ui/icons/Add";
 import Check from "@material-ui/icons/Check";
 import FilterList from "@material-ui/icons/FilterList";
-import Remove from "@material-ui/icons/Remove";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
 import Edit from "@material-ui/icons/Edit";
 import {Modal, TextField, Button} from "@material-ui/core"
 import {makeStyles} from "@material-ui/core/styles"
@@ -22,12 +20,16 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import Avatar from '@material-ui/core/Avatar';
 import EditIcon from '@material-ui/icons/Edit';
+import MenuItem from '@material-ui/core/MenuItem';
+import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
 
 
 const useStyles = makeStyles((theme)=>({
@@ -42,6 +44,9 @@ const useStyles = makeStyles((theme)=>({
     left:'50%',
     transform: 'translate(-50%, -50%)'
   },
+  inputs:{
+    width:"100%"
+  }
 
 }))
 
@@ -67,28 +72,28 @@ const columnas = [
 },{
   title: 'Falta Documentacion',
   field: 'doc'
+},{
+  title: 'Tipo de usuario',
+  field: 'rank'
 },
 ]
-const dataAdmin =[
-  {ID: 1, apellido:"rolon", DNI:42567678,  nombres:"Lautaro Emanuel", fecha: "00-00-0000", edad:21, doc: "true"},
-]
-const dataProfe =[
-  {ID: 1, apellido:"Gimenez", DNI:42567678, nombres:"Luana", fecha: "00-00-0000", edad:43, doc: "true"},
-  {ID: 2, apellido:"Ovelar", DNI:42567678,  nombres:"Hector", fecha: "0o-00-0000", edad:36, doc: "true"},
-  {ID: 3, apellido:"rolon", DNI:42567678,  nombres:"Lautaro Emanuel", fecha: "00-00-0000", edad:21, doc: "true"},
-  {ID: 4, apellido:"Soryu", DNI:42567678,  nombres:"Asuka Langley", fecha: "00-00-0000", edad:28, doc: "true"},
-  {ID: 5, apellido:"Gardner", DNI:42567678, nombres:"Gardner", fecha: "00-00-0000", edad:24, doc: "true"},
-  {ID: 6, apellido:"Ditter", DNI:42567678,  nombres:"Federico Yamil", fecha: "00-00-0000", edad:19, doc: "true"},
-  {ID: 7, apellido:"Silva", DNI:42567678,  nombres:"Roberto Eduardo", fecha: "00-00-0000", edad:21, doc: "true"},
-]
-const dataAlumno =[
-  {ID: 1, apellido:"Megia", DNI:42567678, nombres:"Celeste", fecha: "00-00-0000", edad:20, doc: "true"},
-  {ID: 2, apellido:"Ortiz", DNI:42567678,  nombres:"Benito", fecha: "00-00-0000", edad:19, doc: "true"},
-  {ID: 3, apellido:"Fernandez", DNI:42567678,  nombres:"Leandro", fecha: "00-00-0000", edad:21, doc: "true"},
-  {ID: 4, apellido:"Rodriges", DNI:42567678,  nombres:"David Ezequiel", fecha: "00-00-0000", edad:19, doc: "true"},
-  {ID: 5, apellido:"Almeida", DNI:42567678, nombres:"Martin", fecha: "00-00-0000", edad:20, doc: "true"},
-  {ID: 6, apellido:"Veron", DNI:42567678,  nombres:"Facundo", fecha: "00-00-0000", edad:19, doc: "true"},
-  {ID: 7, apellido:"Perez", DNI:42567678,  nombres:"Rodrigo", fecha: "00-00-0000", edad:21, doc: "true"},
+  
+const data=[
+  {ID: 1, apellido:"Megia", DNI:42567678, nombres:"Celeste", fecha: "00-00-0000", edad:20, doc: "true",rank:'Estudiante'},
+  {ID: 2, apellido:"Ortiz", DNI:42567678,  nombres:"Benito", fecha: "00-00-0000", edad:19, doc: "true",rank:'Estudiante'},
+  {ID: 3, apellido:"Fernandez", DNI:42567678,  nombres:"Leandro", fecha: "00-00-0000", edad:21, doc: "true",rank:'Estudiante'},
+  {ID: 4, apellido:"Rodriges", DNI:42567678,  nombres:"David Ezequiel", fecha: "00-00-0000", edad:19, doc: "true",rank:'Estudiante'},
+  {ID: 5, apellido:"Almeida", DNI:42567678, nombres:"Martin", fecha: "00-00-0000", edad:20, doc: "true",rank:'Estudiante'},
+  {ID: 6, apellido:"Veron", DNI:42567678,  nombres:"Facundo", fecha: "00-00-0000", edad:19, doc: "true",rank:'Estudiante'},
+  {ID: 7, apellido:"Perez", DNI:42567678,  nombres:"Rodrigo", fecha: "00-00-0000", edad:21, doc: "true",rank:'Estudiante'},
+  {ID: 8, apellido:"Gimenez", DNI:42567678, nombres:"Luana", fecha: "00-00-0000", edad:43, doc: "true",rank:'Profesor'},
+  {ID: 9, apellido:"Ovelar", DNI:42567678,  nombres:"Hector", fecha: "0o-00-0000", edad:36, doc: "true",rank:'Profesor'},
+  {ID: 10, apellido:"rolon", DNI:42567678,  nombres:"Lautaro Emanuel", fecha: "00-00-0000", edad:21, doc: "true",rank:'Profesor'},
+  {ID: 11, apellido:"Soryu", DNI:42567678,  nombres:"Asuka Langley", fecha: "00-00-0000", edad:28, doc: "true",rank:'Profesor'},
+  {ID: 12, apellido:"Gardner", DNI:42567678, nombres:"Rachel", fecha: "00-00-0000", edad:24, doc: "true",rank:'Profesor'},
+  {ID: 13, apellido:"Ditter", DNI:42567678,  nombres:"Federico Yamil", fecha: "00-00-0000", edad:19, doc: "true",rank:'Administrador'},
+  {ID: 14, apellido:"Silva", DNI:42567678,  nombres:"Roberto Eduardo", fecha: "00-00-0000", edad:21, doc: "true",rank:'Administrador'},
+  {ID: 15, apellido:"rolon", DNI:42567678,  nombres:"Lautaro Emanuel", fecha: "00-00-0000", edad:21, doc: "true",rank:'Administrador'},
 ]
 
 export default function DAdminUsers() {
@@ -96,15 +101,73 @@ export default function DAdminUsers() {
 
   const [modal1, setModal1] = useState(false);
   const [modal2, setModal2] = useState(false);
+  const [modal3, setModal3] = useState(false);
 
   const abrirCerrarModalEliminar =()=>{
     setModal1(!modal1);
   }
-
   const abrirCerrarModalEditar =()=>{
     setModal2(!modal2);
   }
+  const abrirCerrarModalAñadir =()=>{
+    setModal3(!modal3);
+  }
 
+  const bodyAdd =(
+    <div className={styles.modal}>
+      <DialogTitle id="alert-dialog-title3">
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={styles.avatar}>
+            R
+          </Avatar>
+        }
+        title={<Typography variant="h5" gutterBottom>Agregar Usuario</Typography>}
+      />
+      <CardMedia
+        className={styles.media}
+        image="/static/images/cards/paella.jpg"
+        title="Paella dish"
+      />{/*<Avatar alt="" src="/" className={classes.large} /> Agregar Usuario*/}
+      </DialogTitle>
+      <DialogContent>
+        <form className={styles.form} noValidate autoComplete="off">
+          <div/><TextField className={styles.inputs} required id="standard-required" label="Nombre/s" defaultValue="" />
+          <div/><TextField className={styles.inputs} required id="standard-required" label="Apellido" defaultValue="" />
+          <div/><TextField className={styles.inputs} required id="standard-required" label="Edad" defaultValue="" />                       
+          <div/><TextField className={styles.inputs} required id="standard-required" label="DNI" defaultValue="" maxlength="16"/>
+          <div/><TextField className={styles.inputs} required id="standard-required" label="Dirección" defaultValue="" />
+          <div/><TextField className={styles.inputs} required id="standard-required" label="E-Mail" defaultValue="" /><br/><br/>
+          <div/><InputLabel className={styles.inputs} required id="demo-simple-select-label">Tipo de usuario</InputLabel>
+          <Select
+            className={styles.inputs}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={""}
+            onChange={""}
+            >
+          <MenuItem value={10}>Estudiante</MenuItem>
+          <MenuItem value={20}>Profesor</MenuItem>
+          <MenuItem value={30}>Administrador</MenuItem>
+          </Select> 
+        </form>
+      </DialogContent>
+      <DialogActions>
+          <Button onClick={()=>abrirCerrarModalAñadir()} color="primary">
+              Cancelar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={styles.button}
+            startIcon={<AddIcon />}
+            onClick={()=>abrirCerrarModalAñadir()}
+          >
+            Agregar
+          </Button>
+      </DialogActions>
+    </div>
+  )
   const bodyEdit = (
     <div className={styles.modal} >
       <DialogTitle id="alert-dialog-title1">
@@ -142,7 +205,7 @@ export default function DAdminUsers() {
             color="primary"
             className={styles.button}
             startIcon={<EditIcon />}
-            onClick={()=>abrirCerrarModalEditar()()}
+            onClick={()=>abrirCerrarModalEditar()}
           >
             Editar
           </Button>
@@ -179,14 +242,23 @@ export default function DAdminUsers() {
   return(
   <Wrapper>
   <Wrapper>
+    <Button
+      variant="contained"
+      color="primary"
+      className={styles.button}
+      startIcon={<AddIcon />}
+      onClick={()=>abrirCerrarModalAñadir()}
+    >
+      Añadir nuevo Usuario
+    </Button>
    <MaterialTable
     columns={columnas}
-    data={dataAlumno}
-    title='Usuarios Alumnos'
+    data={data}
+    title='Usuarios'
     actions={[
       {
         tooltip:'Eliminar',
-        icon : Remove,
+        icon : DeleteIcon,
         onClick : ()=>abrirCerrarModalEliminar()
     },
     {
@@ -200,7 +272,7 @@ export default function DAdminUsers() {
   icons={{ 
     Check: Check,
     DetailPanel: ChevronRight,
-    Delete: DeleteOutline,
+    Delete: DeleteIcon,
     Export: SaveAlt,
     Filter: FilterList,
     FirstPage: FirstPage,
@@ -208,7 +280,7 @@ export default function DAdminUsers() {
     NextPage: ChevronRight,
     PreviousPage: ChevronLeft,
     Search: Search,
-    ThirdStateCheck: Remove,
+    ThirdStateCheck: DeleteIcon,
     Add: Add,
     SortArrow: ArrowDownward,
     Clear: Clear,
@@ -243,8 +315,9 @@ export default function DAdminUsers() {
     }
   }}
    >
-   </MaterialTable>
+    </MaterialTable>
   </Wrapper>
+ {/*
   <Wrapper>
    <MaterialTable
     columns={columnas}
@@ -373,8 +446,8 @@ export default function DAdminUsers() {
       }
     }}
    >
-   </MaterialTable>
-  </Wrapper>
+  </MaterialTable>
+  </Wrapper>*/}
   <Modal
     open={modal1}
     onClose={abrirCerrarModalEliminar}
@@ -386,6 +459,12 @@ export default function DAdminUsers() {
     onClose={abrirCerrarModalEditar}
     >
     {bodyEdit}
+  </Modal>
+  <Modal
+    open={modal3}
+    onClose={abrirCerrarModalAñadir}
+    >
+    {bodyAdd}
   </Modal>
   </Wrapper>
  )
