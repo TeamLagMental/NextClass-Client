abf900186673b757b56c66ea32c7be8d03aec742
RobertoSilva1 <69914704+RobertoSilva1@users.noreply.github.com>
2020-12-09 21:05:31 -0300

    tabla-terminada

--- src/screens/dashboard/admin/DAdminUsers.js
+++ src/screens/dashboard/admin/DAdminUsers.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React , {useState} from 'react';
 import MaterialTable from 'material-table';
 import { Wrapper} from './../../../components/dashboard'
 import Search from "@material-ui/icons/Search";
@@ -16,7 +16,34 @@ import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import Clear from "@material-ui/icons/Clear";
 import DeleteOutline from "@material-ui/icons/DeleteOutline";
 import Edit from "@material-ui/icons/Edit";
+import {Modal, TextField, Button} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core/styles"
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Avatar from '@material-ui/core/Avatar';
+import EditIcon from '@material-ui/icons/Edit';
+import Typography from '@material-ui/core/Typography';
 
+
+const useStyles = makeStyles((theme)=>({
+  modal:{
+    position : 'absolute',
+    width : '700',
+    backgroundColor : 'white',
+    border : '2 px solid #000',
+    boxShadow : theme.shadows[5],
+    padding: theme.spacing(2,4,3,4),
+    top:'50%',
+    left:'50%',
+    transform: 'translate(-50%, -50%)'
+  },
+
+}))
 
 const columnas = [
  {
@@ -65,7 +92,90 @@ const dataAlumno =[
 ]
 
 export default function DAdminUsers() {
+  const styles = useStyles();
 
+  const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false);
+
+  const abrirCerrarModalEliminar =()=>{
+    setModal1(!modal1);
+  }
+
+  const abrirCerrarModalEditar =()=>{
+    setModal2(!modal2);
+  }
+
+  const bodyEdit = (
+    <div className={styles.modal} >
+      <DialogTitle id="alert-dialog-title1">
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={styles.avatar}>
+              R
+            </Avatar>
+          }
+          title={<Typography variant="h5" gutterBottom>Editar Usuario</Typography>}
+        />
+        <CardMedia
+          className={styles.media}
+          image="/static/images/cards/paella.jpg"
+          title="Paella dish"
+        />{/*<Avatar alt="" src="/" className={classes.large} /> Agregar Usuario*/}
+      </DialogTitle>
+      <DialogContent>
+        <form className={styles.form} noValidate autoComplete="off">
+                <div/><TextField className={styles.inputs} required id="standard-required" label="Nombre/s" defaultValue="Federico" />
+                <div/><TextField className={styles.inputs} required id="standard-required" label="Apellido" defaultValue="Ditter" />
+                <div/><TextField className={styles.inputs} required id="standard-required" label="Edad" defaultValue="19" />                       
+                <div/><TextField className={styles.inputs} required id="standard-required" label="DNI" defaultValue="433322421" maxlength="16"/>
+                <div/><TextField className={styles.inputs} required id="standard-required" label="Dirección" defaultValue="" />
+                <div/><TextField className={styles.inputs} required id="standard-required" label="E-Mail" defaultValue="fedepitter@hotmail.com" /><br/>
+                <div/><TextField className={styles.inputs} required id="standard-required" label="Tipo de Usuario" defaultValue="Estudiante" />
+        </form>             
+      </DialogContent>
+      <DialogActions>
+          <Button onClick={()=>abrirCerrarModalEditar()} color="primary">
+              Cancelar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={styles.button}
+            startIcon={<EditIcon />}
+            onClick={()=>abrirCerrarModalEditar()()}
+          >
+            Editar
+          </Button>
+      </DialogActions>
+    </div>
+  )
+
+  const bodyDelete = (
+    <div className={styles.modal}>
+      <DialogTitle id="alert-dialog-title2">
+          Eliminar Usuario
+      </DialogTitle>
+      <DialogContent>
+          <DialogContentText id="alert-dialog-description2">
+  ¿Estas seguro que deseas eliminar al usuario vegeta777?
+          </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+          <Button onClick={()=>abrirCerrarModalEliminar()} color="primary">
+              Cancelar
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={styles.button}
+            startIcon={<DeleteIcon />}
+            onClick={()=>abrirCerrarModalEliminar()}
+          >
+            Eliminar
+          </Button>
+      </DialogActions>
+    </div>
+  )
   return(
   <Wrapper>
   <Wrapper>
@@ -77,12 +187,12 @@ export default function DAdminUsers() {
       {
         tooltip:'Eliminar',
         icon : Remove,
-        onClick :  (event, rowData) =>alert("esta eliminando al usuario Alumno :"+rowData.apellido+' '+rowData.nombres)
+        onClick : ()=>abrirCerrarModalEliminar()
     },
     {
       tooltip:'Editar',
       icon : Edit,
-      onClick : (event, rowData) =>alert("esta editando al usuario alumno : "+rowData.apellido+' '+rowData.nombres) 
+      onClick : (event, rowData) =>abrirCerrarModalEditar() 
       //con  (event, rowData) => 
       //en rowData te trae los datos de la fila que seleccionaste
     }
@@ -144,12 +254,12 @@ export default function DAdminUsers() {
       {
         tooltip:'Eliminar',
         icon : Remove,
-        onClick : (event, rowData) =>alert("esta eliminando al usuario Profesor :"+rowData.apellido+' '+rowData.nombres)
+        onClick : ()=>abrirCerrarModalEliminar()
       },
       {
         tooltip:'Editar',
         icon : Edit,
-        onClick : (event, rowData) =>alert("esta editando al usuario Profesor:"+rowData.apellido+' '+rowData.nombres)
+        onClick : ()=>abrirCerrarModalEditar()
       }
     ]}
     icons={{ 
@@ -209,12 +319,12 @@ export default function DAdminUsers() {
       {
         tooltip:'Eliminar',
         icon : Remove,
-        onClick : (event, rowData) =>alert("esta eliminando al usuario administrador :"+rowData.apellido+' '+rowData.nombres)
+        onClick : ()=>abrirCerrarModalEliminar()
       },
       {
         tooltip:'Editar',
         icon : Edit,
-        onClick :(event, rowData) =>alert("esta editando al Administrador :"+rowData.apellido+' '+rowData.nombres)
+        onClick :()=>abrirCerrarModalEditar()
       }
     ]}
     icons={{ 
@@ -265,6 +375,18 @@ export default function DAdminUsers() {
    >
    </MaterialTable>
   </Wrapper>
+  <Modal
+    open={modal1}
+    onClose={abrirCerrarModalEliminar}
+    >
+    {bodyDelete}
+  </Modal>
+  <Modal
+    open={modal2}
+    onClose={abrirCerrarModalEditar}
+    >
+    {bodyEdit}
+  </Modal>
   </Wrapper>
  )
 }
